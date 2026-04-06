ed4fa8f82d3fb7619ea98896521c50e840f29664
erpapp706-ux <erpapp706@gmail.com>
2026-04-06 09:07:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4991,7 +4991,7 @@ def main():
     elif st.session_state.menu == "📊 Avance Cosecha":
         mostrar_avance_cosecha()
     
-    elif st.session_state.menu == "❄️ Envíos a Enfriado":
+    elif st.session_state.menu == "❄️ Traslado a Cámara Fría":
         mostrar_envios_enfriado()
     
     elif st.session_state.menu == "🗑️ Gestión Merma":
